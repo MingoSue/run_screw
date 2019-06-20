4781a3df0ebbcd38976d556563d55c239c769dda
Mingo <www.kxmd@qq.com>
2019-06-20 16:27:35 +0800

    change database settings

--- ScrewDriver/settings.py
+++ ScrewDriver/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'screw_test',
+        'NAME': 'screw',
         'USER': 'root',
         'PASSWORD': 'mingo',
         # 'HOST': '192.168.122.123',
