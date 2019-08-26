8837f20026e12d1d8ce9566e1689e18c13bcc93c
Mingo <www.kxmd@qq.com>
2019-08-26 09:54:07 +0800

    iteration

--- control/position.py
+++ control/position.py
@@ -1,248 +1,12 @@
-# 平面定位第一版
-import os
-import pytz
+# 平面定位第一版, 步数模式
 import sys
 import json
-import csv
 import serial
 from time import sleep
 import can
 from threading import Thread
 from gpiozero import LED, DigitalInputDevice
 import time
-
-import django
-from django.utils import timezone
-from django.db.models import Avg
-
-sys.path.append('..')
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ScrewDriver.settings")
-
-django.setup()
-
-from control.models import Records, ScrewConfig, Weight
-
-
-def get_current_time(datetimenow=None, naive_datetime=False, customtimezone=None):
-    timezone_datetime = datetimenow
-    if datetimenow and not datetimenow.tzinfo:
-        # change naive datetime to datetime with timezone
-        # use timezone.localize will include day light saving to get more accurate timing
-        timezone_datetime = pytz.timezone(os.environ.get('TZ')).localize(datetimenow)
-    tz = None
-    if customtimezone:
-        try:
-            tz = pytz.timezone(customtimezone)
-        except:
-            pass
-
-    # convert to datetime with user local timezone
-    converted_datetime = timezone.localtime(timezone_datetime or timezone.now(), tz)
-    # return datetime converted
-    return converted_datetime.replace(tzinfo=None) if naive_datetime else converted_datetime
-
-
-class Motor:
-
-    def __init__(self, can_channel, motor_id):
-        """
-        Initialization of can motors
-        :param can_channel:name of can device
-        :param motor_id:can id of motor
-        """
-        self.bus = can.interface.Bus(
-            channel=can_channel, bustype='socketcan_ctypes')
-        self.speed = 0
-        self.now_speed = 0
-        self.position = 0
-        self.current = 0
-        self.motor_id = motor_id
-        self.weight = 0
-        self.weight_z = 0
-        self.left_limit = 0
-        self.right_limit = 0
-        self.up_limit = 0
-        self.down_limit = 0
-        self.ser = serial.Serial('/dev/ttyUSB0', baudrate=57600)
-        self.refresh_run()
-
-    def refresh_run(self):
-        t = Thread(target=self.refresh, name='refresh_can')
-        t.setDaemon(True)
-        t.start()
-        t2 = Thread(target=self.serial_refresh, name='refresh_serial')
-        t2.setDaemon(True)
-        t2.start()
-        t3 = Thread(target=self.button_refresh, name='refresh_button')
-        t3.setDaemon(True)
-        t3.start()
-        t4 = Thread(target=self.limit_refresh, name='refresh_limit')
-        t4.setDaemon(True)
-        t4.start()
-        print('four thread  ok~')
-
-    def limit_refresh(self):
-        left = DigitalInputDevice(6)
-        right = DigitalInputDevice(13)
-        up = DigitalInputDevice(26)
-        down = DigitalInputDevice(19)
-        while True:
-            self.left_limit = left.value
-            self.right_limit = right.value
-            self.up_limit = up.value
-            self.down_limit = down.value
-            sleep(0.01)
-
-    def button_refresh(self):
-        b_out = DigitalInputDevice(27)
-        b_in = DigitalInputDevice(17)
-        bin_pre = 0
-        bout_pre = 0
-        while True:
-            bin_now = b_in.value
-            bout_now = b_out.value
-            sleep(0.01)
-            bin_now2 = b_in.value
-            bout_now2 = b_out.value
-
-            if bin_now and bin_now2:
-                bin_now = 1
-            else:
-                bin_now = 0
-
-            if bout_now and bout_now2:
-                bout_now = 1
-            else:
-                bout_now = 0
-            if bin_now and not bin_pre:
-                print('power on ppp')
-                poweron_p()
-            elif bout_now and not bout_pre:
-                print('power on nnn')
-                poweron_n()
-            elif not any([bin_now, bout_now]) and any([bin_pre, bout_pre]):
-                sleep(0.1)
-                print('poweroff...')
-                # poweroff()
-            else:
-                pass
-            bin_pre = bin_now
-            bout_pre = bout_now
-            sleep(0.3)
-
-    def serial_refresh(self):
-        p = LED(21)
-        p.on()
-        while True:
-            self.ser.write([0x32, 0x03, 0x00, 0x50, 0x00, 0x02, 0xC4, 0x1A])
-            sleep(0.05)
-            weight_data = self.ser.read_all()
-            try:
-                weight = round(int('0x' + weight_data.hex()[10:14], 16) * 0.001, 3)
-                if weight >= 10:
-                    weight = 0
-            except Exception as e:
-                print('errorsssssssssssss', e)
-                weight = 0
-            try:
-                with open('weight.json', 'w') as f:
-                    json.dump({'weight': weight}, f)
-
-                with open('adjust_screw_config.json', 'r') as f:
-                    config = json.load(f)
-
-                if weight > config['n']:
-                    self.weight = weight
-                    # print('ssssssssssss> max s : {}'.format(weight))
-
-                    # protect io
-                    p.off()
-                    print('ppppppppppppp')
-                    sleep(0.1)
-                    p.on()
-            except Exception as e:
-                print('error ssss22222222', e)
-
-            # get weight_z value
-            self.ser.write([0x01, 0x03, 0x00, 0x50, 0x00, 0x02, 0xC4, 0x1A])
-            sleep(0.05)
-            weight_data_m = self.ser.read_all()
-            try:
-                weight_z = round(int('0x' + weight_data_m.hex()[10:14], 16) * 0.01, 3)
-                if weight_z >= 10:
-                    # print('////////////>>>', weight_z)
-                    weight_z = 0
-            except Exception as e:
-                print('errorzzzzzzzzzzzzzz', e)
-                weight_z = 0
-            try:
-                if weight_z > 1:
-                    self.weight_z = weight_z
-                    # print('zzzzzzzzzzzzzzzz> max z : {}'.format(weight_z))
-            except Exception as e:
-                print('error zzzzzz2222222222', e)
-
-    def refresh(self):
-        while True:
-            data = self.bus.recv()
-            if data.arbitration_id != 0x1b:
-                continue
-            data = data.data
-            if data[3] > 0xee:
-                now_speed = (data[3] - 0xff) * 256 + (data[2] - 0xff)
-            else:
-                now_speed = data[2] | (data[3] << 8)
-            self.now_speed = now_speed
-            self.current = data[0] | (data[1] << 8)
-            self.position = data[4] | (data[5] << 8) | (data[6] << 16) | (data[7] << 24)
-            # print(self.current, self.now_speed, self.position)
-            direction = 1 if self.now_speed > 0 else -1
-            # sleep(0.1)
-            screw_data = {'speed': self.now_speed, 'current': self.current if self.current < 10000 else 0,
-                          'direction': direction}
-            with open('screw.json', 'w') as f:
-                json.dump(screw_data, f)
-            sleep(0.001)
-
-    def send(self, aid, data):
-        print(time.ctime() + 'can data {}'.format(data))
-        msg = can.Message(arbitration_id=aid, data=data, extended_id=False)
-        self.bus.send(msg, timeout=1)
-        sleep(0.01)
-
-    def speed_mode(self, speed):
-        """
-        :param speed: integer, -255 to 255, speed of left motor, positive speed will go forward,
-                           negative speed will go backward
-        :return:
-        """
-        s_speed = speed & 0xff
-        times = speed >> 8 & 0xff
-        self.send(self.motor_id, [s_speed, times])
-
-
-def poweron_p():
-    with open('adjust_screw_config.json', 'r') as f:
-        config = json.load(f)
-    config.update({'power': 1, "direction": 1})
-    with open('adjust_screw_config.json', 'w') as f:
-        json.dump(config, f)
-
-
-def poweron_n():
-    with open('adjust_screw_config.json', 'r') as f:
-        config = json.load(f)
-    config.update({'power': 1, "direction": -1})
-    with open('adjust_screw_config.json', 'w') as f:
-        json.dump(config, f)
-
-
-def poweroff():
-    with open('adjust_screw_config.json', 'r') as f:
-        config = json.load(f)
-    config.update({'power': 0})
-    with open('adjust_screw_config.json', 'w') as f:
-        json.dump(config, f)
 
 
 class MotorX:
@@ -322,7 +86,7 @@ class MotorZ:
         sleep(0.01)
 
     def speed_mode(self, speed):
-        # speed > 0 forward, speed < 0 backward, speed = 0 stop
+        # speed > 0 forward, speed < 0 backward, speed = 0 stop, should < 1000, more bigger more slower
         self.run_data[2] = 0x23 if speed > 0 else 0x24
         if speed > 0:
             self.run_speed_mode(speed)
@@ -344,7 +108,6 @@ class MotorZ:
 
 def main():
     x = MotorX('can0', 0xc1)
-    z = MotorZ('can0', 0xc2)
     y = MotorX('can0', 0xc3)
 
     # 设置xy轴运行速度等级
@@ -356,15 +119,6 @@ def main():
     y_step = 48300
     # x轴调整步数
     step_add = 2500
-
-    can_motors = Motor('can0', 0x13)
-
-    # 手/自动模式待机状态参数
-    p = 0
-    q = 0
-
-    # 循环次数
-    m = 0
 
     # x轴左右移动位置参数
     step = 0
@@ -392,19 +146,6 @@ def main():
     #     print('config error', e)
     config_1 = {"speed": 1, "speed2": 350, "direction": 1, "n": 1, "n2": 1, "power": 0, "auto": 0, "position": 0,
                 "screw_type": "test001"}
-    # 当前螺丝名称/型号
-    screw_type = config_1['screw_type']
-
-    # 当前螺丝名称/型号所对应的最优缓冲速度
-    settled_list = Records.objects.filter(screw_type=screw_type, is_settled=True).distinct().order_by('-actual_speed')
-    actual_speed = int(settled_list[0].actual_speed) if settled_list else 50
-
-    # 螺丝刀运行速度
-    speed1 = 304
-    # z轴相应的运行速度，speed2 should < 1000
-    speed2 = 500
-
-    direction = 1
 
     while True:
 
@@ -786,33 +527,6 @@ def main():
                                 break
             # 手动
             else:
-                # 关闭自动模式后的复位
-                if y_cycle == 1:
-                    y.run(y_step, 1)
-                    # sleep(3)
-                    y_cycle = 0
-                    if a_cycle == 2:
-                        x.run(x_step + step_add, -1)
-                        a_cycle = 1
-                        step = 0
-                    elif a_cycle == 3:
-                        x.run(x_step * 2 + step_add, -1)
-                        a_cycle = 1
-                        step = 0
-                    elif a_cycle == 1:
-                        x.run(step_add, -1)
-                        a_cycle = 1
-                        step = 0
-                else:
-                    if a_cycle == 2:
-                        x.run(x_step, -1)
-                        a_cycle = 1
-                        step = 0
-                    elif a_cycle == 3:
-                        x.run(x_step * 2, -1)
-                        a_cycle = 1
-                        step = 0
-
                 # 位置1
                 if position == 1:
                     print('position...1...')
@@ -1561,6 +1275,12 @@ def main():
                             config.update({'position': 0})
                             with open('adjust_screw_config.json', 'w') as f:
                                 json.dump(config, f)
+
+                y.run(y_step, -1)
+                sleep(3)
+                x.run(x_step, 1)
+                has_run = 1
+                sleep(1)
 
                 # 位置4
                 if position == 4:
